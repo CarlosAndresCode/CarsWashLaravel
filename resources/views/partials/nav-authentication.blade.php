467c88cd986582e44d72a1ad2b9ea181b4aab163
Carlos Andres Pacheco Fuentes <carlosandres4020@gmail.com>
2025-08-25 20:22:05 -0500

    Enhance navigation links: highlight active login and register links

--- resources/views/partials/nav-authentication.blade.php
+++ resources/views/partials/nav-authentication.blade.php
@@ -3,13 +3,13 @@
     @guest
         @if (Route::has('login'))
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                <a class="nav-link {{ Route::is('login') ? 'text-primary' : '' }}" href="{{ route('login') }}">{{ __('Login') }}</a>
             </li>
         @endif
 
         @if (Route::has('register'))
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                <a class="nav-link {{ Route::is('register') ? 'text-primary' : '' }}" href="{{ route('register') }}">{{ __('Register') }}</a>
             </li>
         @endif
     @else
