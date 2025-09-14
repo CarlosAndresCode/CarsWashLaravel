8712e1cec15dccb545f3e31c65e292693e4dd39f
Carlos Andres Pacheco Fuentes <carlosandres4020@gmail.com>
2025-09-14 14:26:30 -0500

    Replace FQCN with import for `Collection` in `OrderCollection` to improve readability.

--- app/Collection/OrderCollection.php
+++ app/Collection/OrderCollection.php
@@ -2,7 +2,9 @@
 
 namespace App\Collection;
 
-class OrderCollection extends \Illuminate\Database\Eloquent\Collection
+use Illuminate\Database\Eloquent\Collection;
+
+class OrderCollection extends Collection
 {
     public function summaryOrders(): array
     {
